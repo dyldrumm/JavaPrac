247d30687793f791d485d0261166c9270c166669
Dylan Drumm <dyl.drumm@gmail.com>
2020-12-08 15:57:56 -0500

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Basics {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter sample input:");
+        String userIn = input.nextLine();
+
+        System.out.println("User input was: " + userIn);
+
+        //Commit test
+    }
+}
